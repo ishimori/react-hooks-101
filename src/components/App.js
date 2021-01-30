668b099dc110687341d251afc043c88a027b3778
ishimori <ishimori@probsc.jp>
2021-01-31 06:44:59 +0900

    refactor events with reducders

--- src/components/App.js
+++ src/components/App.js
@@ -6,8 +6,10 @@ import AppContext from '../contexts/AppContext'
 import reducer from '../reducers' // index.jsは省略できる
 
 function App() {
-
-  const [state, dispatch]  = useReducer(reducer,[])
+  const initialState = {
+    events : []
+  }
+  const [state, dispatch]  = useReducer(reducer,initialState)
 
   return (
     <AppContext.Provider value={{state,dispatch}}>
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,50 +1,5 @@
-import {
-    CREATE_EVENT,
-    DELETE_EVENT,
-    DELETE_ALL_EVENTS
-} from '../actions'
+import {combineReducers} from 'redux'
 
-// action = {
-//     type:'CREATE_EVENT',
-//     title:'2020年オリンピックのお知らせ',
-//     body:'開催されます。つきましては、、、'
-// }
-// # before
-// state = []
-// # after
-// state = [
-//     {id:1, title:'東京オリンピックのお知らせ', body:'開催します。つきましては、、、'},
-// ]
-// # before
-// state = [
-//     {id:1, title:'タイトル1', body:'ボディ1'},
-//     {id:2, title:'タイトル2', body:'ボディ2'},
-//     {id:3, title:'タイトル3', body:'ボディ3'},
-// ]
-//
-// # after
-// state = [
-//     {id:1, title:'タイトル1', body:'ボディ1'},
-//     {id:2, title:'タイトル2', body:'ボディ2'},
-//     {id:3, title:'タイトル3', body:'ボディ3'},
-//     {id:4, title:'東京オリンピックのお知らせ', body:'開催します。つきましては、、、'},
-// ]
+import events from './events'
 
-const events = (state = [], action) => {
-    switch(action.type) {
-        case CREATE_EVENT:
-            const event = {title:action.title, body:action.body}
-            const length = state.length // 配列の長さ
-            const id = length === 0 ? 1 : state[length - 1].id + 1
-
-            return [...state,{id, ...event}]    // idはショートハンド使ってる
-        case DELETE_EVENT:
-
-            return state.filter(event => event.id !== action.id)
-        case DELETE_ALL_EVENTS:
-            return []
-        default:
-            return state
-    }
-}
-export default events
+export default combineReducers({events})
